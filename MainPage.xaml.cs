ab4dde72b087b8035663db0bbf88954559e9cbce
tine <92997362+tinebasilio@users.noreply.github.com>
2024-08-21 23:59:41 +0800

    Update MainPage.xaml.cs

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,14 +9,7 @@
 
         private async void OnSaveChangesClicked(object sender, EventArgs e)
         {
-            string enteredName = NameEntry.Text;
-            string enteredEmail = EmailEntry.Text;
-            DateTime enteredDate = DateofBirthEntry.Date;
-            string enteredBio = BioEntry.Text;
-
-            string confirmationMessage = $"Name: {enteredName}\nEmail: {enteredEmail}\nDate of Birth: {enteredDate.ToShortDateString()} \nBio: {enteredBio}";
-            
-            await DisplayAlert("User Profile Updated!", confirmationMessage, "OK");
+            this.Resources["ConfirmationLabel"] = "Changes have been updated!";
         }
         private void OnChangeColorThemeClicked(object sender, EventArgs e)
         {
